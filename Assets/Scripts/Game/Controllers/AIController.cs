a7da7cc6e1377b42ecf061bc7f57fac4f6121dcc
Kotuseu Dzmitry <dzmitry.kotuseu@holo-one.com>
2023-09-03 13:33:07 +0200

    Adjusted AI logic.

--- Assets/Scripts/Game/Controllers/AIController.cs
+++ Assets/Scripts/Game/Controllers/AIController.cs
@@ -17,7 +17,7 @@ namespace Pong
                 return;
             }
 
-            List<Ball> orderedActiveBalls = activeBalls.OrderBy(ball =>
+            List<KeyValuePair<Ball, float>> ballsWithArrivalTime = activeBalls.ConvertAll(ball =>
             {
                 float ballXSpeed = ball.Speed * ball.MoveDirection.x;
                 float speedPlatformFrontProjection = Vector2.Dot(platform.transform.right, new Vector2(ballXSpeed, 0));
@@ -32,10 +32,19 @@ namespace Pong
                     approximateTimeTillArrival = Mathf.Abs(deltaPosition.x) / Mathf.Abs(ballXSpeed);
                 }
 
-                return approximateTimeTillArrival;
-            }).ToList();
+                return new KeyValuePair<Ball, float>(ball, approximateTimeTillArrival);
+            });
 
-            float targetPositionY = orderedActiveBalls[0].transform.position.y;
+            List<KeyValuePair<Ball, float>> orderedBallsWithArrivalTime = ballsWithArrivalTime.OrderBy(ballData => ballData.Value).ToList();
+
+            float targetPositionY = orderedBallsWithArrivalTime[0].Key.transform.position.y;
+
+            // Foreseeing the future position
+            if (orderedBallsWithArrivalTime[0].Value < 10f)
+            {
+                targetPositionY += orderedBallsWithArrivalTime[0].Key.MoveDirection.y * orderedBallsWithArrivalTime[0].Value * orderedBallsWithArrivalTime[0].Key.Speed;
+            }
+
             float desiredDeltaY = targetPositionY - platform.transform.position.y;
             float deltaYSign = Mathf.Sign(desiredDeltaY);
             float deltaY = deltaYSign * gameSettings.AiPlatformMovementSpeed * Time.deltaTime;
